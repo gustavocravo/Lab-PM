f6625a410e9ddb25c61b00b498961ca8b84b9616
gustavocravo <gustavocravo.profissional@gmail.com>
2024-08-21 21:53:41 -0300

    contiuando projeto de sala

--- Lab PM1/src/Cliente.java
+++ Lab PM1/src/Cliente.java
@@ -1,10 +1,23 @@
 import java.util.ArrayList;
+import java.time.LocalDate;
 
 public class Cliente {
         private int codCliente;
         private String nome;
         private String CPF;
         private String email;
-        private Data dataNasc;
+        private LocalDate dataNasc;
 
+        public Cliente(int codCliente, String nome, String email, LocalDate dataNasc) {
+                this.codCliente = codCliente;
+                this.nome = nome;
+                this.CPF = CPF;
+                this.email = email;
+                this.dataNasc = dataNasc;
+        }
+
+        public void imprimeCliente() {
+                System.out.println("Nome: " + this.nome);
+                System.out.println("Email: " + this.email);
+        }
 }
