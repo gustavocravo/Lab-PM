cb63a8e042cf6c68955c76ff67595ed0f163cd48
gustavocravo <gustavocravo.profissional@gmail.com>
2024-08-21 22:07:18 -0300

    continuacao cliente

--- Lab PM1/src/Cliente.java
+++ Lab PM1/src/Cliente.java
@@ -8,7 +8,7 @@ public class Cliente {
         private String email;
         private LocalDate dataNasc;
 
-        public Cliente(int codCliente, String nome, String email, LocalDate dataNasc) {
+        public Cliente(int codCliente, String nome, String CPF, String email, LocalDate dataNasc) {
                 this.codCliente = codCliente;
                 this.nome = nome;
                 this.CPF = CPF;
--- Lab PM1/src/Teste.java
+++ Lab PM1/src/Teste.java
@@ -5,5 +5,9 @@ public class Teste {
 
         LocalDate date = LocalDate.of(2024, 8, 21);
         System.out.println("Data " + date);
+
+        Cliente cliente1 = new Cliente(1, "Gustavo", "13323456554", "gustavo@gmail", date) {
+        cliente1.imprimeCliente();
+        }
     }
 }
